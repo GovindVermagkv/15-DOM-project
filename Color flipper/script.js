064980210a4858b8e4f8b34d725031875bc8e3ff
GovindVermagkv <sonygovind.gkv740@gmail.com>
2022-08-08 22:41:04 +0530

    animation add

--- Color flipper/script.js
+++ Color flipper/script.js
@@ -1,9 +1,16 @@
 var color=["blue","green","aqua","pink","red"]
 var btn=document.getElementById("btn")
+var imge=document.getElementById("img")
+var imge2=document.getElementById("img2")
+
 function colorchange(){
     randomnumber=getRandomNumber();
     document.body.style.backgroundColor=color[randomnumber]
-   
+    imge.src="1uJP.gif";
+    imge.style.display="block"
+    imge2.src="1uJP.gif";
+    imge2.style.display="block"
+   imge2.style.left="10%"
 
     var head=document.getElementById("colorname");
     head.innerText = color[randomnumber];
@@ -14,3 +21,5 @@ function colorchange(){
 function getRandomNumber(){
     return Math.floor(Math.random()*color.length);
 }
+
+// setInterval(colorchange,200)
