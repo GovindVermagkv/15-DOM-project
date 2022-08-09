64bcfa0eacb5ca83852bbc8d1d67be02f0f55e3b
GovindVermagkv <sonygovind.gkv740@gmail.com>
2022-08-09 13:18:38 +0530

    modified

--- counter/script.js
+++ counter/script.js
@@ -10,27 +10,28 @@ var count2=0;
 
 function upvote(){
     count=count+1;
-    display.textContent=count;
+    up.style.color="blue";
+    // display.textContent=count;
     upshow.textContent=count;
-    display.style.color="green";
+    // display.style.color="green";
     up.style.color="blue";
     audio.play()
   
 }
 function refreshing(){
-    display.textContent="0";
+    refresh.style.color="green";
     upshow.textContent="0";
     downshow.textContent="0";
-    display.style.color="black";
-    refresh.style.color="green";
+   
 }
 
 function downvote(){
     count2=count2+1;
-    display.textContent=count2;
+    audio.play()
+    down.style.color="red";
+    // display.textContent=count2;
     downshow.textContent=count2;
-    display.style.color="red";
     down.style.color="red";
     display.style.transition=".5s";
-    audio.play()
+   
 }
