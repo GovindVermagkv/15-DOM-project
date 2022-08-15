cc0831d1eb5f54f88856edf3ead89377335aaf1b
GovindVermagkv <sonygovind.gkv740@gmail.com>
2022-08-15 16:06:09 +0530

    navbar and sidebar added

---
+++ sidebar/script.js
@@ -0,0 +1,45 @@
+var bars=document.getElementById("menubar")
+var asides=document.getElementById("aside")
+var color=document.getElementById("color")
+var header=document.querySelector("header")
+var nav=document.querySelector("h1")
+var icon=document.querySelector("#color")
+var menubars=document.querySelector("#menubar")
+var menuse=document.querySelectorAll("a")
+var body=document.querySelector("body")
+
+function activeaside(){
+
+    if(asides.style.display=="none"){
+        asides.style.display="block";
+        bars.style.color="red";
+        bars.style.transform="rotate(90deg)";
+    }
+    else{
+        asides.style.display="none";
+        bars.style.color="blue";
+        bars.style.transform="rotate(-90deg)";
+    }
+}
+
+function colors(){
+   if(body.style.backgroundColor !="black"){
+    asides.style.backgroundColor="black";
+    header.style.backgroundColor="black";
+    body.style.backgroundColor="black";
+    asides.style.color="white";
+    nav.style.color="white";
+    icon.style.color="white";
+    menuse.style.color="white";
+    menubars.style.color="white";
+   }
+   else{
+    asides.style.backgroundColor="white";
+    header.style.backgroundColor="white";
+    body.style.backgroundColor="white";
+    asides.style.color="blue";
+    nav.style.color="blue";
+    icon.style.color="blue";
+    menuse.style.color="blue";
+   }
+}
